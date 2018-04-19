7928e409168a0b4ae0a97bd73cc75e91a2c4eeae
Travis Kane <kevinthomaskane@gmail.com>
2018-04-18 18:51:23 -0700

    removed yarn lock file

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ database.once("open", function(){
 
 require("./routes/api.js")(app, db, express);
 
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
